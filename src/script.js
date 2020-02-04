09cd5cbc0fff2113b6f86281f4bbdae44f16335f
Filip <39651273+Kotfil@users.noreply.github.com>
2020-02-04 18:03:15 +0200

    4

---
+++ src/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let marvel = {
+    name: "batmen",
+    age: 30,
+    isWarrior: true
+};
+
+console.log(marvel.name);
+
+let iphone = ['iphone6s.small','iphone7.mid','iphoneXS.high'];
+
+console.log(iphone[0]);
+
+alert('Hi Filippe and Israel Tel Aviv');
+// let answerUser = confirm('Do you man?');
+// console.log(answerUser);
+
+let answerUser = prompt('Do you man? and 18 old', 'Да');
+console.log(typeof(iphone));
