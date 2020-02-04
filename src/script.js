57f8bebde3ba8ad1d65d727638cf6623052130a1
Filip <39651273+Kotfil@users.noreply.github.com>
2020-02-05 01:00:32 +0200

    5

--- src/script.js
+++ src/script.js
@@ -1,20 +1,39 @@
 'use strict';
+s = 2;
+console.log(s);
+// // let marvel = {
+// //     name: "batmen",
+// //     age: 30,
+// //     isWarrior: true
+// // };
 
-let marvel = {
-    name: "batmen",
-    age: 30,
-    isWarrior: true
-};
+// // console.log(marvel.name);
 
-console.log(marvel.name);
+// // let iphone = ['iphone6s.small','iphone7.mid','iphoneXS.high'];
 
-let iphone = ['iphone6s.small','iphone7.mid','iphoneXS.high'];
+// //  console.log(iphone[0]);
 
-console.log(iphone[0]);
+// // alert('Hi Filippe and Israel Tel Aviv');
+// // // let answerUser = confirm('Do you man?');
+// // // console.log(answerUser);
 
-alert('Hi Filippe and Israel Tel Aviv');
-// let answerUser = confirm('Do you man?');
-// console.log(answerUser);
+// // let answerUser = +prompt("Do you man? and 18 old", "Да");
+// //  console.log(typeof(answerUser));
 
-let answerUser = prompt('Do you man? and 18 old', 'Да');
-console.log(typeof(iphone));
+// //  console.log(typeof("iphone" + " - final"));
+//  console.log(typeof(20 + +" - final"));/
+
+let incr = 10,
+    decr = 10;
+
+  
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(2 === "4");
+
+    let Prajska = false,
+        Gagarin = false;
+
+        console.log(Prajska || Gagarin);
