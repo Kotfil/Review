5c9ccc03ce4ec9172dcad2d11b2fe2d2d400fb1a
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2020-02-09 22:34:34 +0200

    9?

--- src/script.js
+++ src/script.js
@@ -17,15 +17,6 @@
 // }
 // console.log(Object.keys(options).length);
 
-const people = [
-    {name: 'Филипп', age: 27, bidget: 40000 },
-    {name: 'Виктория', age: 23, bidget: 90000 },
-    {name: 'Оксана', age: 51, bidget: 40000 },
-    {name: 'Марина', age: 70, bidget: 40000 },
-    {name: 'Виктор', age: 60, bidget: 40000 },
-    {name: 'Светлана', age: 51, bidget: 40000 },
-    {name: 'Чида', age: 20, bidget: 40000 }
-]
 
 // for (let i = 0; i < people.length; i++) {
 //     console.log(people[i])
@@ -43,6 +34,30 @@ const people = [
 
 // people.forEach(person => console.log(person))
 
-const newPeople = people.map (person => {
-    return 'Hellod'})
-    console.log(newPeople)
+// const newPeople = people.map (person => {
+//     return person.name})
+//     console.log(newPeople)
+
+// const adust = people.filter(person => {
+//     if (person.age >= 30) {
+//         return true
+//     }
+// })
+// console.log(adust)
+
+// const adust = people.filter(person => person.age >= 30)
+// console.log(adust)
+
+const people = [
+    {name: 'Филипп', age: 27, bidget: 40000 },
+    {name: 'Виктория', age: 23, bidget: 90000 },
+    {name: 'Оксана', age: 51, bidget: 40000 },
+    {name: 'Марина', age: 70, bidget: 40000 },
+    {name: 'Виктор', age: 60, bidget: 40000 },
+    {name: 'Светлана', age: 51, bidget: 40000 },
+    {name: 'Чида', age: 20, bidget: 40000 }
+]
+
+const vacsis = people.reduce 
+    ((total,person) => { return total + person.budget},0)
+    console.log (vacsis)
