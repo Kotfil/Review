06f3aea1827b8b7d9caa26c78f56a0615a752909
FilippK <filippkotenko@outlook.com>
2020-03-10 19:21:39 +0200

    Complate React JS social app course The Way of Samurai 2

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>
+{
+    return (
+        <header className='header'>
+            <img src='https://www.pngkey.com/png/detail/394-3948444_logo-people-png.png'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () =>
+{
+    return (
+        <div className="content">
+            <div>
+                <img src="https://cdn.dribbble.com/users/925347/screenshots/6281667/image.png"/>
+            </div>
+            <div>ava+description
+            </div>
+            <div>MyPosts
+            </div>
+            <div>New Post
+            </div>
+            <div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
